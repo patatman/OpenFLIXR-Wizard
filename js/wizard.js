0220479a5150d9b4dac3f01b38ec8e0130acc2ea
mfrelink <mfrelink@me.com>
2016-05-15 16:30:05 +0200

    updates

--- js/wizard.js
+++ js/wizard.js
@@ -100,22 +100,6 @@ function validateFirstStep() {
 
     $(".wizard-card form").validate({
         rules: {
-            hostname: {
-                required: true,
-                minlength: 7
-            },
-            ip: {
-                required: true,
-                minlength: 7
-            },
-            subnet: {
-                required: true,
-                minlength: 7
-            },
-            gateway: {
-                required: true,
-                minlength: 7
-            },
             setup: {
                 required: true
             },
@@ -127,54 +111,12 @@ function validateFirstStep() {
                 minlength: 5
             }
 
-
-            /*  other possible input validations
-            			,username: {
-            				required: true,
-            				minlength: 2
-            			},
-            			password: {
-            				required: true,
-            				minlength: 5
-            			},
-            			confirm_password: {
-            				required: true,
-            				minlength: 5,
-            				equalTo: "#password"
-            			},
-
-            			topic: {
-            				required: "#newsletter:checked",
-            				minlength: 2
-            			},
-            			agree: "required"
-            */
-
         },
         messages: {
             setup: "Please select one of the options",
             username: "Please enter a username",
             password: "Please enter a valid password",
             email: "Please enter a valid email address",
-
-            /*   other posible validation messages
-            			username: {
-            				required: "Please enter a username",
-            				minlength: "Your username must consist of at least 2 characters"
-            			},
-            			password: {
-            				required: "Please provide a password",
-            				minlength: "Your password must be at least 5 characters long"
-            			},
-            			confirm_password: {
-            				required: "Please provide a password",
-            				minlength: "Your password must be at least 5 characters long",
-            				equalTo: "Please enter the same password as above"
-            			},
-            			email: "Please enter a valid email address",
-            			agree: "Please accept our policy",
-            			topic: "Please select at least 2 topics"
-            */
 
         }
     });
